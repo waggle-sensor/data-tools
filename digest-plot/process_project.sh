95121507bdc06b789ee942cc592a60de9f7079b9
onetarp <dawnkd@rpi.edu>
2018-07-13 14:13:29 -0500

    build plots and tables don\'t get deleted every time process_project runs anymore

--- digest-plot/process_project.sh
+++ digest-plot/process_project.sh
@@ -3,16 +3,16 @@ find ./scratch -name $1.complete.[1-9]* | xargs -n 1 sh -c './create_project_gra
 find ./scratch -name $1.complete.[1-9]* | xargs -n 1 sh -c 'python3 gen_tables.py $0'
 mkdir -p ./build/plots
 mkdir -p ./build/tables
-rm -rf ./build/plots/*
-rm -rf ./build/tables/*
+# rm -rf ./build/plots/*
+# rm -rf ./build/tables/*
 if [ $(find ./scratch -name *.png | wc -l ) -gt 0 ]; then
-echo "Moving files"
-mv $(find ./scratch -name *.png) ./build/plots
+	echo "Moving files"
+	mv $(find ./scratch -name *.png) ./build/plots
 fi 
 
 if [ $(find ./scratch -name *.html | wc -l | wc -l ) -gt 0 ]; then
-echo "Moving files"
-mv $(find ./scratch -name *.html) ./build/tables
+	echo "Moving files"
+	mv $(find ./scratch -name *.html) ./build/tables
 fi 
 cp $(find ./scratch -name style.css | head -1) ./build/tables
 cat $(find ./scratch -name nodes.csv) | awk 'NR==1 || !/node_id,project_id,vsn,address,lat,lon,description/' > scratch/nodes.csv
