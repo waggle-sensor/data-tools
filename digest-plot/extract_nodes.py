9615522008058a82c15ba0f0ea128fd90afbe390
Beehive Worker <dawnkd@rpi.edu>
2018-07-20 12:19:15 -0500

    fixed error where chicago graphs did not contain all data

--- digest-plot/extract_nodes.py
+++ digest-plot/extract_nodes.py
@@ -12,7 +12,8 @@ from plotting_utilities import getNodes, grep
 if __name__ == '__main__':
 	project_dir = str(sys.argv[1])
 	input_dir = str(sys.argv[2])
-	date = str(sys.argv[3])
+	output_dir = str(sys.argv[3])
+	date = str(sys.argv[4])
 
 	dicts = getNodes(project_dir)
 	nodes_dict = dicts[0]
@@ -20,18 +21,13 @@ if __name__ == '__main__':
 	input_path = os.path.join(input_dir,'data.csv')
 
 	# set up file structure (project_dir/data/node_id/date/date.csv)
-	data_dir = os.path.join(project_dir,'data')
-	if not os.path.exists(data_dir):
-		os.makedirs(data_dir)
+	data_dir = os.path.join(output_dir,'data')
+	os.makedirs(data_dir,exist_ok=True)
 	print('Extracting node data for {}.'.format(date))
 	for node in nodes_dict:
 		node_dir = os.path.join(data_dir,node)
-		if not os.path.exists(node_dir):
-			os.makedirs(node_dir)
-		date_dir = os.path.join(node_dir,date)
-		if not os.path.exists(date_dir):
-			os.makedirs(date_dir)
-		output_path = os.path.join(date_dir,'{}.csv'.format(date))
+		os.makedirs(node_dir,exist_ok=True)
+		os.makedirs(date_dir,exist_ok=True)
 
 		# extract data for a node
 		status = grep(node,input_path,output_path)
