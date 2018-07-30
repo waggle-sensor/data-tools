de639c0b8394186a7943d5ff72048e1c14035e66
onetarp <dawnkd@rpi.edu>
2018-07-30 12:21:56 -0500

    remove data_dates files whenever handle_all_projects runs

--- digest-plot/handle_all_projects.sh
+++ digest-plot/handle_all_projects.sh
@@ -4,6 +4,7 @@ rm -rf ./scratch/*.latest.tar
 rm -rf ./scratch/*.complete.[1-9]*
 rm -rf ./build/plots/*
 rm -rf ./build/tables/*
+rm -rf ./build/data_dates/*
 curl "http://www.mcs.anl.gov/research/projects/waggle/downloads/datasets/index.php" | \
 	tr "<" "\n" | grep complete.latest.tar | cut -d ">" -f 1 | sed "s/a href='.//g" | sed "s/.[a-z]*.latest.tar'//g" | sed 's/\///g' | xargs -n 1 sh -c './process_project.sh $0'
 echo "node_id,project_id,vsn,address,lat,lon,description" > ./build/nodes.csv
