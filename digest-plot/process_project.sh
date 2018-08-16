1353881e3ff567312b984287638ab3a09bbd3a25
Sean Shahkarami <sean.shahkarami@gmail.com>
2018-08-16 14:19:28 -0500

    simplified process project node match

--- digest-plot/process_project.sh
+++ digest-plot/process_project.sh
@@ -11,9 +11,8 @@ find ./scratch/ -name $1.complete.[1-9]* | xargs -n 1 sh -c 'python3 gen_tables.
 if [ $(find ./scratch/ -name *.html | wc -l ) -gt 0 ]; then
 	echo "Moving files"
 	mv $(find ./scratch/ -name *.html) ./build/tables
-fi 
+fi
 cp $(find ./scratch/ -name style.css | head -1) ./build/tables
-cat $(find ./scratch/$1.complete.[1-9]* -name nodes.csv) | awk 'NR==1 || !/node_id,project_id,vsn,address,lat,lon,description/' > scratch/nodes.csv
+cat $(find ./scratch/$1.complete.[1-9]* -name nodes.csv) | awk 'NR==1 || !/node_id,/' > scratch/nodes.csv
 mv ./scratch/nodes.csv ./build/$1_nodes.csv
 rm -rf ./scratch/*
-
