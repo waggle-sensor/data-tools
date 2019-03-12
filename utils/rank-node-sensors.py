f67e2119d11476ee4fc508aa908d162db84a5678
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-03-12 15:29:37 -0500

    added node + sensor breakdown

---
+++ utils/rank-node-sensors.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import pandas as pd
+import sys
+
+df = pd.read_csv(sys.stdin, dtype={'valid': bool})
+
+r = df.groupby(['node_id', 'subsystem', 'sensor', 'parameter'])['valid'].agg(['sum', 'count'])
+
+r['total'] = r['count']
+r['valid'] = r['sum']
+r['invalid'] = r['count'] - r['sum']
+r['valid_ratio'] = r['sum'] / r['count']
+
+with pd.option_context('display.multi_sparse', False):
+    print(r[['total', 'valid', 'invalid', 'valid_ratio']].sort_values(['valid_ratio', 'subsystem', 'sensor', 'parameter']).reset_index().to_string(index=False))
