3d3e68a94479e8595b6c40df8963cfd9a2dc2a01
Sean Shahkarami <sean.shahkarami@gmail.com>
2019-03-12 14:21:13 -0500

    added ranking tools

---
+++ utils/rank-nodes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import pandas as pd
+import sys
+
+df = pd.read_csv(sys.stdin, dtype={'valid': bool})
+
+r = df.groupby('node_id')['valid'].agg(['sum', 'count'])
+
+r['total'] = r['count']
+r['valid'] = r['sum']
+r['invalid'] = r['count'] - r['sum']
+r['ratio'] = r['sum'] / r['count']
+print(r[['total', 'valid', 'invalid', 'ratio']].sort_values('ratio').to_string())
---
+++ utils/rank-sensors.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import pandas as pd
+import sys
+
+df = pd.read_csv(sys.stdin, dtype={'valid': bool})
+
+r = df.groupby(['subsystem', 'sensor', 'parameter'])['valid'].agg(['sum', 'count'])
+
+r['total'] = r['count']
+r['valid'] = r['sum']
+r['invalid'] = r['count'] - r['sum']
+r['ratio'] = r['sum'] / r['count']
+
+with pd.option_context('display.multi_sparse', False):
+    print(r[['total', 'valid', 'invalid', 'ratio']].sort_values(['ratio', 'subsystem', 'sensor', 'parameter']).to_string())
